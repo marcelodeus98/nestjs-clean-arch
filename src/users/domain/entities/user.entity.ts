158d6e33200cc69f39a55e145cf6953a8b0384f4
marcelodeus98 <marcelodeus98@gmail.com>
2025-04-20 20:41:03 -0300

    feat: add getter methods

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -9,6 +9,22 @@ export class UserEntity {
   constructor(public readonly props: UserProps) {
     this.props.createdAt = this.props.createdAt ?? new Date();
   };
+
+  get name() {
+    return this.props.name;
+  };
+
+  get email() {
+    return this.props.email;
+  };
+
+  get password() {
+    return this.props.password;
+  };
+
+  get createdAt() {
+    return this.props.createdAt;
+  };
 };
 
 
